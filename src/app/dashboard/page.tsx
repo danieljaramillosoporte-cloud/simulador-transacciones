8f91aa32a583b0ede1d99bdbabd926dd981918f4
danieljaramillosoporte <danieljaramillosoporte@gmail.com>
2025-10-07 17:11:20 -0500

    Restauro versión correcta de DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,11 +3,13 @@
 import { useSearchParams } from "next/navigation";
 import UserDashboard from "./UserDashboard.tsx";
 
+
 export default function DashboardPage() {
   const params = useSearchParams();
   const curp = params.get("curp"); // lee ?curp=XXXX de la URL
 
   if (!curp) return <div className="p-8">❌ No se recibió CURP</div>;
+
 
   return <UserDashboard curp={curp} />;
 }
